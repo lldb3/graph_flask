54565030055fb8dd151cefa9a19cf18069280c0e
elmø <anon_fwd@icloud.com>
2021-02-17 22:57:33 +0100

    switch to postgre + added d3.js base

--- db_populate.py
+++ db_populate.py
@@ -1,9 +1,5 @@
-import sqlite3
 import json
-from sqlalchemy.orm import sessionmaker
 from model import *
-from base import Session, Base, engine
-
 
 Base.metadata.create_all(engine)
 session = Session()
@@ -14,10 +10,11 @@ f = open("csec_data_full.json", 'r')
 csec_data = json.loads(f.read())
 f.close()
 
+
 for k in csec_data.keys():
     ka = csec_data[k]
     summary = ka['summary']
-    essentials = str(ka['essentials'])
+    essentials = ka['essentials']
     ka_obj = KA(k, summary, essentials)
     session.add(ka_obj)
     
