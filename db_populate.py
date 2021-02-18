98a5e46429259fc8e8312c8d232ddb2ce12f9eea
elmø <anon_fwd@icloud.com>
2021-02-18 21:42:48 +0100

    fix db and populate script

--- db_populate.py
+++ db_populate.py
@@ -15,8 +15,18 @@ for k in csec_data.keys():
     ka = csec_data[k]
     summary = ka['summary']
     essentials = ka['essentials']
-    ka_obj = KA(k, summary, essentials)
-    session.add(ka_obj)
+    ka_db_obj = KA(k, summary, essentials)
+    session.add(ka_db_obj)
+    for outcome in ka['outcomes']:
+        outcome_db_obj = Outcome(outcome['name'], outcome['questions'], ka_db_obj)
+        session.add(outcome_db_obj)
+    for ku in ka['units']:
+        ku_db_obj = KU(ku['name'], ku['description'], ka_db_obj)
+        session.add(ku_db_obj)
+        keys = list(ku.keys())[2:]
+        for topic in keys:
+            topic_db_obj = Topic(topic, ku[topic], ku_db_obj)
+            session.add(topic_db_obj)
     
 
 session.commit()
--- model.py
+++ model.py
@@ -26,48 +26,48 @@ class KA(Base):
 class KU(Base):
     __tablename__ = "knowledge_unit"
     name = Column(String)
-    description = Column(ARRAY(String))
+    description = Column(String)
     id = Column(Integer, primary_key=True)
     ka_id = Column(Integer, ForeignKey("knowledge_area.id"))
     ka = relationship("KA")
 
-    def __init__(self, name, description, ka_id):
+    def __init__(self, name, description, ka):
         self.name = name
         self.description = description
-        self.ka_id = ka_id
+        self.ka = ka
 
 
 class Topic(Base):
     __tablename__ = "topic"
     name = Column(String)
-    description = Column(String)
+    description = Column(ARRAY(String))
     id = Column(Integer, primary_key=True)
     ku_id = Column(Integer, ForeignKey('knowledge_unit.id'))
     ku = relationship("KU")
 
-    def __init__(self, name, description, ku_id):
+    def __init__(self, name, description, ku):
         self.name = name
         self.description = description
         self.name = name
         self.description = description
-        self.ku_id = ku_id
+        self.ku = ku
 
     # TODO implement list of items for the description + spliting on ',' and 'and' keywords
 
 
-class Outcomes(Base):
+class Outcome(Base):
     __tablename__ = "outcomes"
     name = Column(String)
-    description = Column(String)
     id = Column(Integer, primary_key=True)
-    questions = Column(String)
+    questions = Column(ARRAY(String))
     ka_id = Column(Integer, ForeignKey('knowledge_area.id'))
+    ka = relationship("KA")
 
-    def __init__(self, name, description, ka_id):
+    def __init__(self, name, description, ka):
         self.name = name
         self.description = description
         self.questions = self.description_to_questions()
-        self.ka_id = ka_id
+        self.ka = ka
 
     def description_to_questions(self):
         return self.description  # TODO desc to questions
