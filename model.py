d7f297fa7c90d5c5f45ba068841314f0a8da0615
Edgar Munro <meln@posteo.net>
2021-02-18 21:47:29 +0100

    change db name

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgresql://postgres@localhost:5432/postgres', echo=True)
+engine = create_engine('postgresql://postgres@localhost:5432/csec', echo=True)
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
